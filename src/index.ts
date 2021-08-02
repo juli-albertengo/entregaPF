e4257c5eb59b2b35cdd74904a094be1f251a54de
Julieta Albertengo <albertengo.juli@gmail.com>
2021-08-01 22:11:48 -0300

    Changed import of apimessages in index

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ const config = require('./config');
 const express = require('express');
 import { Request, Response } from 'express';
 import path from 'path'
-import {ApiMessages} from '../src/api/api.messages'; 
+import {ApiMessages} from './api/api.messages'; 
 const passport = require('passport');
 const jwtStrategy = require('./middleware/passportAuthMiddleware');
 const {loggerFile} = require('./services/logger');
