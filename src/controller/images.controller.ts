e8af231c7e0156d6e48bd44f85e3f3419e206d8b
Julieta Albertengo <albertengo.juli@gmail.com>
2021-08-01 22:36:51 -0300

    Fixed import statements

--- src/controller/images.controller.ts
+++ src/controller/images.controller.ts
@@ -1,4 +1,4 @@
-const config = require('../config.js');
+const config = require('../config');
 import {Request, Response} from 'express';
 import {ApiImages} from '../api/api.images';
 import {ApiProducts} from '../api/api.products';
